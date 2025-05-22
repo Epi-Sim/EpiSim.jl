cd11a97e385066e8759012fdb5f736575b1ffc91
Miguel Ponce-de-Leon <miguel.ponce@bsc.es>
2025-05-22 17:10:10 +0200

    add Pkg.update() as an optional flag in install.jl

--- install.jl
+++ install.jl
@@ -1,15 +1,11 @@
 using Pkg
 
-# run this in an interactive session in MN5 or it will time-out !!
-# note that you should have installed the dependencies before running this script
-
 Pkg.activate(@__DIR__)
-Pkg.update()
 Pkg.instantiate()
 Pkg.precompile()
 
-using PackageCompiler
 using ArgParse
+using PackageCompiler
 
 function parse_commandline()
     s = ArgParseSettings()
@@ -25,6 +21,10 @@ function parse_commandline()
         "--target", "-t"
             help = "Target folder where the single excecutable will be stored"
             default ="."
+        "--update", "-u"
+            help = "Update dependencies"
+            action = :store_true
+            default = false
     end
     return parse_args(s)
 end
@@ -33,6 +33,12 @@ end
 
 args = parse_commandline()
 @assert isdir(args["target"]) "Target folder $(args["target"]) does not exist"
+
+if args["update"]
+    Pkg.update()
+    Pkg.instantiate()
+    Pkg.precompile()
+end
 
 if args["compile"]
     build_folder = "build"
